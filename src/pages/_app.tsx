512724923a90548259c70dfb7032ec763bf6e6b1
PoornaGTX <onlinepoorna@gmail.com>
2024-04-30 18:42:26 +0530

    T3 app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
+
+import { api } from "~/utils/api";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "~/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <main className={`font-sans ${inter.variable}`}>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </main>
+  );
+};
+
+export default api.withTRPC(MyApp);
